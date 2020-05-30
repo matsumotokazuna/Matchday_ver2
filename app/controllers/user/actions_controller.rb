4f666c371be9148001e83564b4077e6dc2b4044a
vagrant <vagrant@localhost.localdomain>
2020-05-30 10:01:16 +0000

    [add]actions

--- app/controllers/user/actions_controller.rb
+++ app/controllers/user/actions_controller.rb
@@ -1,22 +1,2 @@
 class User::ActionsController < User::Base
-    def create
-        @action = Action.new(action_params)
-        @action.save
-    end
-
-    def update
-        @action = Action.find(params[:id])
-        @action.update(action_params)
-    end
-
-    private
-    def action_params
-        params.require(:action).permit(
-            :male_user_id,
-            :female_user_id,
-            :male_matching_at,
-            :female_matching_at,
-            :matching_at
-        )
-    end
 end
--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -12,20 +12,40 @@ class User::UsersController < User::Base
     def show
         @user = User.find(params[:id])
         if current_user.gender_cd == "男性" #ログインユーザが男性の場合
-            @action_exist_check = Action.find_by(male_user_id: current_user.id, female_user_id: @user.id)
-            if @action_exist_check == nil #actionが未作成の場合
-                @action = Action.new
-            else #actionが既に存在する場合
-                @action = @action_exist_check
-            end
+            @action = Action.find_by(male_user_id: current_user.id, female_user_id: @user.id)
         else #ログインユーザが女性の場合
-            @action_exist_check = Action.find_by(male_user_id: @user.id, female_user_id: current_user.id)
-            if @action_exist_check == nil #actionが未作成の場合
-                @action = Action.new
-            else #actionが既に存在する場合
-                @action = @action_exist_check
-            end
+            @action = Action.find_by(male_user_id: @user.id, female_user_id: current_user.id)
         end
     end
 
+    def create_action
+        @user = User.find(params[:id])
+        @action = Action.new
+        if current_user.gender_cd == "男性" #ログインユーザが男性の場合
+            @action.male_user_id = current_user.id
+            @action.female_user_id = @user.id
+            @action.male_matching_at = DateTime.now
+        else #ログインユーザが女性の場合
+            @action.male_user_id = @user.id
+            @action.female_user_id = current_user.id
+            @action.female_matching_at = DateTime.now
+        end
+        @action.save
+        redirect_to user_path(@user.id)
+    end
+
+    def update_action
+        @user = User.find(params[:id])
+        if  current_user.gender_cd == "男性" #ログインユーザが男性の場合
+            @action = Action.find_by(male_user_id: current_user.id, female_user_id: @user.id)
+            @action.male_matching_at = DateTime.now
+            @action.matching_at = DateTime.now
+        else #ログインユーザが女性の場合
+            @action = Action.find_by(male_user_id: @user.id, female_user_id: current_user.id)
+            @action.female_matching_at = DateTime.now
+            @action.matching_at = DateTime.now
+        end
+        @action.save
+        redirect_to user_path(@user.id)
+    end
 end
