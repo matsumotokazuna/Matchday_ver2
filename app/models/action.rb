b3d35461fba6dc0069c6b3dab7f2a952ee8f3191
vagrant <vagrant@localhost.localdomain>
2020-05-31 13:25:18 +0000

    [fix]actions

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,2 +1,7 @@
 class Action < ApplicationRecord
+    belongs_to :user, foreign_key: "male_user_id"
+    belongs_to :user, foreign_key: "female_user_id"
+
+    belongs_to :male_user, :class_name => 'User', :foreign_key => 'male_user_id'  
+    belongs_to :female_user, :class_name => 'User', :foreign_key => 'female_user_id'  
 end
